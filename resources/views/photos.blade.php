15235e394458c90f25205f650278e2954e8f5305
dassepascal <pascaldasse56@gmail.com>
2022-10-03 19:53:52 +0200

    affichage cote gauche et droite

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -3,12 +3,18 @@
 
     <div class="articles row justify-content-center ">
         @foreach ($photos as $photo)
-
+{{-- @dump($photo->category->id) --}}
                 <div class="col-md-6">
             <div class="card my-3">
+@if ($photo->category->id === 2)
+<img src="{{ Storage::url($photo->path)}}" alt="#">
+@endif
+@if ($photo->category->id === 1)
+<img src="{{ Storage::url($photo->path)}}" alt="#">
+@endif
 
-                    <img src="{{ Storage::url($photo->path)}}" alt="#">
-                
+
+
             </div>
         </div>
         @endforeach
