16d9506ad630474383bb732650c5eadc3aa76e31
dassepascal <pascaldasse56@gmail.com>
2022-10-05 11:52:15 +0200

    init slide

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,9 @@
 require('./bootstrap');
-let action = document.getElementsByName('.action');
-console.log(action);
+
+var carousel = document.querySelector('.carousel-item').className='carousel-item active';
+
+
+var carouselDroit = document.getElementById('carouselDroit');
+carouselDroit.children[0].firstElementChild.className ='carousel-item active'
+console.log(carouselDroit.children[0].firstElementChild);
 
--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -1,54 +1,33 @@
 <section>
     <div class="row">
-        <div class="col-md-6 bg-primary">
-
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        @foreach ($photos as $photo)
-                        {{-- @dump($photo) --}}
-                        @if ($photo->category_id ==1)
-                        @if ($photo->id ==1)
-                        <div class="carousel-item active">
-                            <img src="{{ Storage::url($photo->path)}}" alt="#" class="d-block w-100">
-                        </div>
-                        @elseif($photo->id >1)
-                        <div class="carousel-item">
-                            <img src="{{ Storage::url($photo->path) }}" alt="" class="d-block w-100">
-                        </div>
-                        @endif
-                        @endif
-                        @endforeach
-
-                    </div>
-                </div>
-
-        </div>
-        <div class="col-md-6 bg-primary">
-
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
+        <div class="col-md-6 ">
+            <div id="carouselGauche" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner ">
                     @foreach ($photos as $photo)
                     {{-- @dump($photo) --}}
-                    @if ($photo->category_id ==2)
-                    @if ($photo->id )
-                    <div class="carousel-item active">
+                    @if ($photo->category_id==1)
+                    <div class="carousel-item ">
                         <img src="{{ Storage::url($photo->path)}}" alt="#" class="d-block w-100">
                     </div>
-                    @elseif($photo->id >1)
-                    <div class="carousel-item">
-                        <img src="{{ Storage::url($photo->path) }}" alt="" class="d-block w-100">
-                    </div>
-                    @endif
                     @endif
                     @endforeach
-
                 </div>
             </div>
-
-    </div>
-
-    </div>
+        </div>
 
 
-
+        <div class="col-md-6 d-flex bg-info justify-content-center align-items-center ">
+            <div id="carouselDroit" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner ">
+                    @foreach ($photos as $photo)
+                    {{-- @dump($photo) --}}
+                    @if ($photo->category_id==2)
+                    <div class="carousel-item ">
+                        <img src="{{ Storage::url($photo->path)}}" alt="#" class="d-block ">
+                    </div>
+                    @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>
 </section>
