2d66cec1b958eee2dad503fd8e6db9fee61fc139
dassepascal <pascaldasse56@gmail.com>
2022-10-05 12:38:32 +0200

    init js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
 require('./bootstrap');
 
+var carousel = document.querySelector('.carousel-item').className='carousel-item active';
+
+
+var carouselDroit = document.getElementById('carouselDroit');
+carouselDroit.children[0].firstElementChild.className ='carousel-item active'
 
